16aa03ee796d1f24a540f228321323d12019e48e
zooboss <sandrozz@yandex.ru>
2016-12-02 13:43:25 +0300

    Added database connection (uses blog.sql), some functions (just copied), removed all fake articles div's from main_page.php.
    Next step - relise article text from db

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
-//require_once(dirname(__FILE__) . "/models/functions.php");
+require_once(dirname(__FILE__)."/models/database.php");
+$link = db_connect();
+
+require_once(dirname(__FILE__) . "/models/functions.php");
 include(dirname(__FILE__) . "/views/main_page.php");
 
 ?>
