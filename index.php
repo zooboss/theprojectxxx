7a49f5e9d0345122d70110013301ff3f75644e57
zooboss <sandrozz@yandex.ru>
2016-12-02 12:58:56 +0300

    copy index to branch

--- index.php
+++ index.php
@@ -1,1 +1,393 @@
-<?php echo''Hello ' ?>
+<!DOCTYPE html>
+
+<html>
+
+<head> 
+
+<title>      </title> <!-- Придумать название -->
+
+<link href='' rel='icon' type='image/x-icon'/>	<!-- сгенерировать фавиконы для всех устройств --> 
+<link type='text/css' rel='stylesheet' href='css/style.css' />
+<link type='text/css' rel='stylesheet' href='css/font-awesome.css' />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+</head>
+    
+<body>   
+
+<div itemscope='itemscope' itemtype='http://schema.org/Blog' class="invisible">  <!-- Прописать в css display: none -->
+<meta content='' itemprop='name'/>    <!-- Придумать название это тег для микроразметки и поисковиков -->
+</div>
+
+<div class='top-navigation'>
+
+<div class='navlist'>
+    <ul>
+      <li class='selected'><a href='#0'>Главная</a></li>
+        <li><a href='#0'>About</a></li>
+          <li><a href='#0'>Contact Us</a></li>
+    </ul>
+</div>
+
+<div class='search-box'>
+<span class='icon-search'>
+<i class='fa fa-search'></i>
+</span>
+<form action='http://gridz-themexpose.blogspot.ru/search' method='get'>
+<input name='q' type='search' value='Search and hit enter'/>
+</form>
+</div>
+
+<!-- подобрать иконки -->
+<div class='share-box'>
+<a class='social-facebook' href='#' target='_blank'><i class='fa fa-facebook'></i></a>
+<a class='social-twitter' href='#' target='_blank'><i class='fa fa-twitter'></i></a>
+<a class='social-gplus' href='#' target='_blank'><i class='fa fa-google-plus'></i></a>
+<a class='social-linkedin' href='#' target='_blank'><i class='fa fa-linkedin'></i></a>
+<a class='social-pinterest' href='#' target='_blank'><i class='fa fa-pinterest'></i></a>
+<a class='social-youtube' href='#' target='_blank'><i class='fa fa-youtube'></i></a>
+<a class='social-vimeo' href='#' target='_blank'><i class='fa fa-vimeo-square'></i></a>
+<a class='social-instagram' href='#' target='_blank'><i class='fa fa-instagram'></i></a>
+</div>
+
+</div>
+
+<header>
+
+<div class="col-md-6 col-md-offset-3">   
+
+<h1> Название нашего сайта для СЕО, можно скрыть </h1>   
+
+</div>
+
+</header>      
+    
+	
+<div class='container-fluid'>
+
+<!-- отдельный блок статьи-->
+    <div class='grid-item col-md-3'>                           
+<div class='image-wrap'>
+<img alt='#0' src='#0'/>
+</div>
+
+
+<div class='post-body'>
+ 
+<div class='post-author'>
+   
+   <div class='image-thumb'>
+   <img alt='#0' title='#0' src='#0'/>
+   </div>
+   <div class='name-author'>
+   <cite>Балабол Балаболов</cite>
+   </div>
+
+</div>
+
+<div class='post-title'>
+<h2><a href='#0'>Статья о хорошем</a></h2>
+</div>
+
+<div class='post-entry'>
+<p>Сюда мы вставим php скрипт с выводом текста...</p>
+</div>
+
+
+<div class='postfooter clearfix'>
+ <div class='socialpost'>
+  <div class='icons clearfix'>
+   <a href='#0'><i class='fa fa-facebook'></i><div class='texts'>Facebook</div></a>
+   <a href='#0'><i class='fa fa-vk'></i><div class='texts'>VK</div></a>
+   <a href='#0'><i class='fa fa-twitter'></i><div class='texts'>Twitter</div></a>
+   </div>
+</div>
+<a href='#0'><div class='read'>Читать </div></a>
+</div>
+
+<div class='linker clearfix'>
+<i class='fa fa-comment'></i>
+48 Comments
+<button type="button" class="btn btn-info col-md-offset-4">Выразить мнение</button>
+</div>
+
+
+</div>
+</div>   <!-- grid-item col-md-3 -->  
+<!-- отдельный блок статьи-->
+
+
+
+
+    <div class='grid-item col-md-3'>                           
+<div class='image-wrap'>
+<img alt='#0' src='#0'/>
+</div>
+
+
+<div class='post-body'>
+ 
+<div class='post-author'>
+   
+   <div class='image-thumb'>
+   <img alt='#0' title='#0' src='#0'/>
+   </div>
+   <div class='name-author'>
+   <cite>Балабол Балаболов</cite>
+   </div>
+
+</div>
+
+<div class='post-title'>
+<h2><a href='#0'>Статья о хорошем</a></h2>
+</div>
+
+<div class='post-entry'>
+<p>Сюда мы вставим php скрипт с выводом текста...</p>
+</div>
+
+
+<div class='postfooter clearfix'>
+ <div class='socialpost'>
+  <div class='icons clearfix'>
+   <a href='#0'><i class='fa fa-facebook'></i><div class='texts'>Facebook</div></a>
+   <a href='#0'><i class='fa fa-vk'></i><div class='texts'>VK</div></a>
+   <a href='#0'><i class='fa fa-twitter'></i><div class='texts'>Twitter</div></a>
+   </div>
+</div>
+<a href='#0'><div class='read'>Читать </div></a>
+</div>
+
+<div class='linker clearfix'>
+<i class='fa fa-comment'></i>
+48 Comments
+<button type="button" class="btn btn-info col-md-offset-4">Выразить мнение</button>
+</div>
+
+
+</div>
+</div>   <!-- grid-item col-md-3 -->
+
+
+
+    <div class='grid-item col-md-3'>                           
+<div class='image-wrap'>
+<img alt='#0' src='#0'/>
+</div>
+
+
+<div class='post-body'>
+ 
+<div class='post-author'>
+   
+   <div class='image-thumb'>
+   <img alt='#0' title='#0' src='#0'/>
+   </div>
+   <div class='name-author'>
+   <cite>Балабол Балаболов</cite>
+   </div>
+
+</div>
+
+<div class='post-title'>
+<h2><a href='#0'>Статья о хорошем</a></h2>
+</div>
+
+<div class='post-entry'>
+<p>Сюда мы вставим php скрипт с выводом текста...</p>
+</div>
+
+
+<div class='postfooter clearfix'>
+ <div class='socialpost'>
+  <div class='icons clearfix'>
+   <a href='#0'><i class='fa fa-facebook'></i><div class='texts'>Facebook</div></a>
+   <a href='#0'><i class='fa fa-vk'></i><div class='texts'>VK</div></a>
+   <a href='#0'><i class='fa fa-twitter'></i><div class='texts'>Twitter</div></a>
+   </div>
+</div>
+<a href='#0'><div class='read'>Читать </div></a>
+</div>
+
+<div class='linker clearfix'>
+<i class='fa fa-comment'></i>
+48 Comments
+<button type="button" class="btn btn-info col-md-offset-4">Выразить мнение</button>
+</div>
+
+
+</div>
+</div>   <!-- grid-item col-md-3 -->
+
+
+
+
+    <div class='grid-item col-md-3'>                           
+<div class='image-wrap'>
+<img alt='#0' src='#0'/>
+</div>
+
+
+<div class='post-body'>
+ 
+<div class='post-author'>
+   
+   <div class='image-thumb'>
+   <img alt='#0' title='#0' src='#0'/>
+   </div>
+   <div class='name-author'>
+   <cite>Балабол Балаболов</cite>
+   </div>
+
+</div>
+
+<div class='post-title'>
+<h2><a href='#0'>Статья о хорошем</a></h2>
+</div>
+
+<div class='post-entry'>
+<p>Сюда мы вставим php скрипт с выводом текста...</p>
+</div>
+
+
+<div class='postfooter clearfix'>
+ <div class='socialpost'>
+  <div class='icons clearfix'>
+   <a href='#0'><i class='fa fa-facebook'></i><div class='texts'>Facebook</div></a>
+   <a href='#0'><i class='fa fa-vk'></i><div class='texts'>VK</div></a>
+   <a href='#0'><i class='fa fa-twitter'></i><div class='texts'>Twitter</div></a>
+   </div>
+</div>
+<a href='#0'><div class='read'>Читать </div></a>
+</div>
+
+<div class='linker clearfix'>
+<i class='fa fa-comment'></i>
+48 Comments
+<button type="button" class="btn btn-info col-md-offset-4">Выразить мнение</button>
+</div>
+
+
+</div>
+</div>   <!-- grid-item col-md-3 -->
+
+
+
+    <div class='grid-item col-md-3'>                           
+<div class='image-wrap'>
+<img alt='#0' src='#0'/>
+</div>
+
+
+<div class='post-body'>
+ 
+<div class='post-author'>
+   
+   <div class='image-thumb'>
+   <img alt='#0' title='#0' src='#0'/>
+   </div>
+   <div class='name-author'>
+   <cite>Балабол Балаболов</cite>
+   </div>
+
+</div>
+
+<div class='post-title'>
+<h2><a href='#0'>Статья о хорошем</a></h2>
+</div>
+
+<div class='post-entry'>
+<p>Сюда мы вставим php скрипт с выводом текста...</p>
+</div>
+
+
+<div class='postfooter clearfix'>
+ <div class='socialpost'>
+  <div class='icons clearfix'>
+   <a href='#0'><i class='fa fa-facebook'></i><div class='texts'>Facebook</div></a>
+   <a href='#0'><i class='fa fa-vk'></i><div class='texts'>VK</div></a>
+   <a href='#0'><i class='fa fa-twitter'></i><div class='texts'>Twitter</div></a>
+   </div>
+</div>
+<a href='#0'><div class='read'>Читать </div></a>
+</div>
+
+<div class='linker clearfix'>
+<i class='fa fa-comment'></i>
+48 Comments
+<button type="button" class="btn btn-info col-md-offset-4">Выразить мнение</button>
+</div>
+
+
+</div>
+</div>   <!-- grid-item col-md-3 -->
+
+
+
+    <div class='grid-item col-md-3'>                           
+<div class='image-wrap'>
+<img alt='#0' src='#0'/>
+</div>
+
+
+<div class='post-body'>
+ 
+<div class='post-author'>
+   
+   <div class='image-thumb'>
+   <img alt='#0' title='#0' src='#0'/>
+   </div>
+   <div class='name-author'>
+   <cite>Балабол Балаболов</cite>
+   </div>
+
+</div>
+
+<div class='post-title'>
+<h2><a href='#0'>Статья о хорошем</a></h2>
+</div>
+
+<div class='post-entry'>
+<p>Сюда мы вставим php скрипт с выводом текста...</p>
+</div>
+
+
+<div class='postfooter clearfix'>
+ <div class='socialpost'>
+  <div class='icons clearfix'>
+   <a href='#0'><i class='fa fa-facebook'></i><div class='texts'>Facebook</div></a>
+   <a href='#0'><i class='fa fa-vk'></i><div class='texts'>VK</div></a>
+   <a href='#0'><i class='fa fa-twitter'></i><div class='texts'>Twitter</div></a>
+   </div>
+</div>
+<a href='#0'><div class='read'>Читать </div></a>
+</div>
+
+<div class='linker clearfix'>
+<i class='fa fa-comment'></i>
+48 Comments
+<button type="button" class="btn btn-info col-md-offset-4">Выразить мнение</button>
+</div>
+
+
+</div>
+</div>   <!-- grid-item col-md-3 -->
+
+
+
+               
+</div>   <!-- col-md-9-->
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	</body>
+</html>
