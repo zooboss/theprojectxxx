debc58ec457c38a0348452783b2a3dbf47f5000e
zooboss <sandrozz@yandex.ru>
2016-12-02 13:16:16 +0300

    trying to add local libraries

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <title>      </title> <!-- Придумать название -->
 
 <link href='' rel='icon' type='image/x-icon'/>	<!-- сгенерировать фавиконы для всех устройств --> 
+<link type='text/css' rel='stylesheet' href='libs/css/bootstrap.css' />
 <link type='text/css' rel='stylesheet' href='css/style.css' />
 <link type='text/css' rel='stylesheet' href='css/font-awesome.css' />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
