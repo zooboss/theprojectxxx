c13ea0ad92636c6e2ed4183d0eee560f213b6472
zooboss <sandrozz@yandex.ru>
2017-06-21 15:44:09 +0300

    Изменена файловая структура, удалены лишние файлы и убрано слово template из названия. Изменено оформление и эффекты кнопки "отправить", добавлены специальные классы оформления (серый, зеленый), добавляемые и убираемые signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,29 @@
+//Плавающие названия полей регистрации
+
+jQuery(document).ready(function($){
+	if( $('.floating-labels').length > 0 ) floatLabels();
+
+	function floatLabels() {
+		var inputFields = $('.floating-labels .cd-label').next();
+		inputFields.each(function(){
+			var singleInput = $(this);
+			//check if user is filling one of the form fields 
+			checkVal(singleInput);
+			singleInput.on('change keyup', function(){
+				checkVal(singleInput);	
+			});
+		});
+	}
+
+	function checkVal(inputField) {
+		( inputField.val() == '' ) ? inputField.prev('.cd-label').removeClass('float') : inputField.prev('.cd-label').addClass('float');
+	}
+});
+
+
+//Функционал проверки данных регистрации
+
+jQuery(document).ready(function($){
 var login,
 	email,
 	password,
@@ -10,7 +36,7 @@ var login,
 $(function() {
 	//Логин
 	$("#login").change(function(){
-		login = $("#login").val();
+        login = $("#login").val();
 		var expLogin = /^[а-яА-ЯёЁa-zA-Z0-9]+$/g;
 		var resLogin = login.search(expLogin);
 		if(resLogin == -1){
@@ -20,7 +46,7 @@ $(function() {
 			buttonOnAndOff();
 		}else{
 			$.ajax({
-			url: "testLoginEmail.php",
+			url: "../models/registration/testLoginEmail.php",
 			type: "GET",
 			data: "login=" + login,
 			cache: false,
@@ -37,6 +63,7 @@ $(function() {
 		});
 			loginStat = 1;
 			buttonOnAndOff();
+            console.log("buttononoff_event");
 		}
 		
 	});
@@ -58,7 +85,7 @@ $(function() {
 		}else{
 			
 			$.ajax({
-			url: "testLoginEmail.php",
+			url: "../models/registration/testLoginEmail.php",
 			type: "GET",
 			data: "email=" + email,
 			cache: false,			
@@ -127,12 +154,14 @@ $(function() {
 	});
 	
 	function buttonOnAndOff(){
-		if(emailStat == 1 && passwordStat == 1 && password2Stat == 1 && loginStat == 1){
-			$("#submit").removeAttr("disabled");
+        console.log(emailStat, passwordStat, password2Stat, loginStat);
+        if(emailStat == 1 && passwordStat == 1 && password2Stat == 1 && loginStat == 1){
+			$("#submit").removeAttr("disabled").removeClass().addClass("btnEnabled");
 		}else{
-			$("#submit").attr("disabled","disabled");
+			$("#submit").attr("disabled","disabled").removeClass().addClass("btnDisabled");
 		}
 	
 	}
 	
+});
 });
--- views/signup_page.php
+++ views/signup_page.php
@@ -3,69 +3,141 @@
 
 ?>
 
-<!DOCTYPE html>
-<html>
+
+<!doctype html>
+<html lang="en" class="no-js">
 <head>
-    <title>Signup | Coding Cage</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="/theprojectxxx/libs/css/pickmeup.css" type="text/css" />	
-    <link type='text/css' rel='stylesheet' href='/theprojectxxx/css/style.css' />
-    <script type="text/javascript" src="/theprojectxxx/libs/js/pickmeup.js"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script type='text/javascript' src="/theprojectxxx/js/signup.js"></script>
-    
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="/theprojectxxx/css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="/theprojectxxx/css/signup.css"> <!-- Resource style -->
+	<script src="/theprojectxxx/libs/js/modernizr.js"></script> <!-- Modernizr -->
+    <script src="/theprojectxxx/libs/js/jquery-2.1.1.js"></script> <!-- JQuery -->
+    <script src="/theprojectxxx/js/signup.js"></script>
+  	
+	<title>Contact Form | CodyHouse</title>
 </head>
- 
-<script type="text/javascript">
+<body>
+	<form class="cd-form floating-labels" method="post">
+		<fieldset>
+			<legend>Регистрация</legend>
 
-</script>
+			<!--<div class="error-message">
+				<p>Пожалуйста, введите правильный email адрес</p>
+			--></div>
 
-  <body >
-	<?php if(isset($msg)) echo $msg;?>
+			<div class="icon">
+				<label class="cd-label" for="login">Логин</label>
+				<input class="user" type="text"  id="login" name="txtuname" required>
+		    </div> 
+		    
+		    <div class="icon">
+		    	<label class="cd-label" for="email">Адрес email</label>
+				<input class="email error" type="email" id='email' name="txtemail" required>
+		    </div>
+		    
+		    <div class="icon">
+		    	<label class="cd-label" for="password">Пароль</label>
+				<input class="company" type="password" id='password' name="txtpass" required> <!-- type="text" -->
+		    </div> 
+		    
+		    <div class="icon">
+		    	<label class="cd-label" for="password2">Подтвердите пароль</label>
+				<input class="company" type="password" id='password2' name="txtpass_check" required> <!-- type="text" -->
+		    </div> 
+		    
+		</fieldset>
 
-      <form  method="post">
-        <h2 class="form-signin-heading">Sign Up TEST</h2><hr />
-		<label><font color='red'>*</font> Ваш Логин:<br></label>
-        <input type="text" class="input-block-level" id='login' placeholder="Логин" name="txtuname" required /> <span></span> <br> 
-		
-		<label><font color='red'>*</font> Ваш E-mail:<br></label>
-        <input type="text" class="input-block-level" id='email' placeholder="Адрес электронной почты" name="txtemail" required /> <span></span> <br>
-        
-		<label><font color='red'>*</font> Ваш пароль:<br></label>
-		<input type="password" class="input-block-level" id='password' placeholder="Пароль" name="txtpass" required /> <span></span> <br>
-		
-		<label><font color='red'>*</font> Подтвердите пароль:<br></label>
-		<input type="password" class="input-block-level" id='password2' placeholder="Проверка пароля" name="txtpass_check" required /> <span></span>  <br>
-	    
-		<input type="radio" name="gender" value="Мужской"/>мужской
-        <input type="radio" name="gender" value="Женский"/> женский <br>
-  	Дата рождения(гггг.мм.дд): <input type="datetime" name='birthdate' id='date' value='' /> 
-		Номер телефона (полный с кодом страны)<input type="phone" name='phone_number'  value='' /> <br>
-		<input type="text" class="input-block-level" placeholder="Имя" name="name" /> <br>
-		<input type="text" class="input-block-level" placeholder="Фамилия" name="surname"  /> <br>
-		<input type="text" class="input-block-level" placeholder="Отчество" name="patronymic"  /> 
-  		
-       	<hr />
-       <input type="submit" name="btn-signup" value="Зарегистрироваться" id="submit" disabled>
-        <!--<a href="index.php" style="float:right;" class="btn btn-large">Sign In</a>-->
-      </form>
-	  
-<script type='text/javascript'>
-addEventListener('DOMContentLoaded', function () {
-	pickmeup('#date', {
-		position       : 'right',
-		hide_on_select : true
-	});
-});
-</script>
+		<fieldset>
+			<legend>Личные данные</legend>
 
+			<!--<div>
+				<h4>Ваш пол</h4>
 
-<!--Проверка пароля, логина и всего остального -->
+				<p class="cd-select icon">
+					<select class="budget">
+						<option value="0">Select Budget</option>
+						<option value="1">&lt; $5000</option>
+						<option value="2">$5000 - $10000</option>
+						<option value="3">&gt; $10000</option>
+					</select>
+				</p>
+			--></div> 
 
-	  
+			<div>
+				<h4>Ваш пол</h4>
 
+				<ul class="cd-form-list">
+					<li>
+						<input type="radio" name="gender" value="Мужской" id="gender_male" checked>
+						<label for="gender_male">Мужской</label>
+					</li>
+						
+					<li>
+						<input type="radio" name="gender" value="Женский" id="gender_female">
+						<label for="gender_female">Женский</label>
+					</li>
+					
+				</ul>
+			</div>
+			
+            <div class="icon">
+				<label class="cd-label" for="date">Дата рождения</label>
+				<input class="user" type="text" name='birthdate' id='date' value='' >  <!-- type="datetime" -->
+		    </div> 
+            
+            <div class="icon">
+		    	<label class="cd-label" for="phone_number_id">Номер телефона</label>
+				<input class="company" type="text" name='phone_number'  value='' id="phone_number_id"> <!-- type="phone" -->
+		    </div> 
+		    
+		    <div class="icon">
+				<label class="cd-label" for="name_id">Имя</label>
+				<input class="user" type="text" name="name" id="name_id" >
+		    </div> 
+		    <div class="icon">
+				<label class="cd-label" for="surname_id">Фамилия</label>
+				<input class="user" type="text" name="surname" id="surname_id" >
+		    </div> 
+		    <div class="icon">
+				<label class="cd-label" for="patronymic_id">Отчество</label>
+				<input class="user" type="text" name="patronymic" id="patronymic_id" >
+		    </div> 
 
-	
+			<!--<div>
+				<h4>Features</h4>
 
-  </body>
+				<ul class="cd-form-list">
+					<li>
+						<input type="checkbox" id="cd-checkbox-1">
+						<label for="cd-checkbox-1">Option 1</label>
+					</li>
+
+					<li>
+						<input type="checkbox" id="cd-checkbox-2">
+						<label for="cd-checkbox-2">Option 2</label>
+					</li>
+
+					<li>
+						<input type="checkbox" id="cd-checkbox-3">
+						<label for="cd-checkbox-3">Option 3</label>
+					</li>
+				</ul>
+			</div>
+
+			<div class="icon">
+				<label class="cd-label" for="cd-textarea">Project description</label>
+      			<textarea class="message" name="cd-textarea" id="cd-textarea" required></textarea>
+			--></div>
+
+			<div>
+		      	<input type="submit" name="btn-signup" value="Отправить" id="submit" class="btnDisabled" disabled>
+		    </div>
+		</fieldset>
+	</form>
+
+</body>
 </html>
