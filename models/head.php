f67aa51fecee4b0c1e05363f89716962a0aa7664
Batka-mahno <19920430@mail.ru>
2017-07-11 15:08:12 +0300

    Объединение комментариев и профиля

--- models/head.php
+++ models/head.php
@@ -2,11 +2,12 @@
 <title>      </title> <!-- Придумать название -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="UTF-8">
-<link href='#0' rel='icon' type='image/x-icon'/> <!-- сгенерировать фавиконы для всех устройств --> 
+<link href='#0' rel='icon' type='image/x-icon'/>	<!-- сгенерировать фавиконы для всех устройств --> 
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
 <link type='text/css' rel='stylesheet' href='/theprojectxxx/libs/css/bootstrap.css' />  <!-- локальное подключение для запуска на апаче -->
 <link type='text/css' rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link type='text/css' rel='stylesheet' href='/theprojectxxx/libs/css/font-awesome.css' />
+<link type='text/css' rel='stylesheet' href='/theprojectxxx/libs/css/comments.css' />
 <!--<link rel="stylesheet" href="/theprojectxxx/css/reset.css"> <!-- CSS reset -->
 <link rel="stylesheet" href="/theprojectxxx/css/signup.css"> <!-- Resource style -->
 <link type='text/css' rel='stylesheet' href='/theprojectxxx/css/style.css' />
@@ -18,3 +19,4 @@
 <script src="/theprojectxxx/js/signup.js"></script>
 
 </head>
+
