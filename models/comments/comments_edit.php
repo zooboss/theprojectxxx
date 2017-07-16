363ef8341ded1e99695c1c3decfb6f0860a5a9fc
zooboss <sandrozz@yandex.ru>
2017-07-16 14:58:34 +0300

    Комментарии отображаются со стилями при отправке.

--- models/comments/comments_edit.php
+++ models/comments/comments_edit.php
@@ -1,4 +1,4 @@
-﻿<?php 
+<?php 
 require_once ( $_SERVER['DOCUMENT_ROOT'] . "/theprojectxxx/models/comments/comments.php");
 $article_comments = new COMMENTS();		
  if(isset($_POST['form_data'])){
@@ -45,10 +45,24 @@ $article_comments = new COMMENTS();
   foreach ($stmt as $com)
         {
             ?>
-            <p>Автор:<?php echo $com['author']; ?> </p>
-            <p>Комментарий:<?php echo $com['content']; ?> </p> 
-            <p>Дата:<?php echo $com['date']; ?> </p> 
-            <p>Дата:<?php echo $com['time']; ?> </p> 
+            <div class = "single-comment">
+               <div class = "single-comment-avatar">
+                   <img src = "/theprojectxxx/img/icons/full_user.jpg" class = "single-comment-avatar-image">
+               </div>
+               <div class = "single-comment-body">
+                   <div class = "single-comment-header">
+                       <a href = "#0"><?php echo $com['author']?></a>
+                       <span><?php echo $com['date'] . " " . $com['time'] ?></span>
+                   </div>
+                   <div class = "single-comment-text">
+                      <span><?php echo $com['content']; ?></span>  
+                   </div>
+                   <div class = "single-comment-footer">
+                       <a href = "#0">Ответить</a>
+                   </div>                
+                </div>
+                
+            </div>
            <?php
         }
 ?>
