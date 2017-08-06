034c1e74a80e2260ae5394c44c5ccf9e46b9032b
zooboss <sandrozz@yandex.ru>
2017-08-06 13:56:57 +0300

    Мелкий косяк пользовательского меню

--- models/header.php
+++ models/header.php
@@ -46,7 +46,7 @@
                              <img class = "login-icon" src = "/theprojectxxx/img/icons/login_icon.svg">
                              Профиль
                          </a>
-                         <div class="dropdown-divider"></div>
+                         <!--<div class="dropdown-divider"></div>-->
                          <a href = "/theprojectxxx/models/registration/logout.php" class = "dropdown-item btn">
                              <img class = "login-icon" src = "/theprojectxxx/img/icons/logout_icon.svg">
                              Выйти
