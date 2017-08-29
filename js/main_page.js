964b8af5eca99c2e4bbebf777ba1a7ec1d818061
zooboss <sandrozz@yandex.ru>
2017-08-29 10:15:54 +0300

    Работает, но генерирует без конца

--- js/main_page.js
+++ js/main_page.js
@@ -1,21 +1,22 @@
 $( document ).ready(function() {    
 
 /* Первый запуск главной страницы */    
-    
+    var articlesInBlock = 6;
     $(document).on('ready', function(){
-        var articlesInBlock = 11;
+        
         
         $.ajax({
           
           url: "models/main_page/main_page_gallery.php", 
           type: "POST",
           data: {
+              requestType: "initial_request",
               articlesInBlock: articlesInBlock
           },
           dataType: 'json',
           success: function(json){
             $('#articlesGallery').replaceWith(json); // заменим форму данными, полученными в ответе
-            console.log("articles_loaded");
+            
             
           },
           error: function(xhr, status, error){
@@ -26,21 +27,44 @@ $( document ).ready(function() {
     });
     
     /* INFINITE SCROLL  */
-     function element_in_scroll(elem){
-         var docViewTop = $(window).scrollTop();
-         var docViewBottom = docViewTop + $(window).height();
+    var blockNumber = 1;
+    function element_in_scroll(elem){
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
          
-         var elemTop = $(elem).offset().top;
-         var elemBottom = elemTop + $(elem).height();
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
          
-         return((elemBottom <= docViewBottom));
+        return((elemBottom <= docViewBottom));
          
-     }
+    }
     
     $(document).on("scroll", function(e){
-       console.log(element_in_scroll(".columns"));
+       
+        if(element_in_scroll('#block-'+blockNumber) == true){
+            blockNumber++;
         
-        
+            $.ajax({
+
+              url: "models/main_page/main_page_gallery.php", 
+              type: "POST",
+              data: {
+                  requestType: "scroll_request",
+                  blockNumber: blockNumber,
+                  articlesInBlock: articlesInBlock
+              },
+              dataType: 'json',
+              success: function(json){
+                //$('#block-'+(blockNumber-1)).append(json);
+               $('#articlesGallery').append(json);
+
+              },
+              error: function(xhr, status, error){
+                  console.log(xhr.responseText);;
+              }
+            });
+            
+        }
     });
     
     
