3b8f561aa9828b96770a7e972a03457d73d11931
zooboss <sandrozz@yandex.ru>
2017-09-02 22:58:59 +0300

    Оформлена страница статьи

--- views/article_page.php
+++ views/article_page.php
@@ -1,6 +1,9 @@
 <?php
 
 require_once ( $_SERVER['DOCUMENT_ROOT'] . "/theprojectxxx/models/comments/comments.php"); 
+require_once ( $_SERVER['DOCUMENT_ROOT'] . "/theprojectxxx/models/functions.php"); 
+
+
 
 ?>
 
@@ -10,7 +13,7 @@ require_once ( $_SERVER['DOCUMENT_ROOT'] . "/theprojectxxx/models/comments/comme
     <?php include_once( $_SERVER['DOCUMENT_ROOT'] . "/theprojectxxx/models/head.php"); ?>
     <script src="/theprojectxxx/js/comments.js"></script>
 </head>
-<body>
+<body class = "article-page-body">
    <?php 
     include_once( $_SERVER['DOCUMENT_ROOT'] . "/theprojectxxx/models/header.php");  
     ?>
@@ -29,13 +32,16 @@ require_once ( $_SERVER['DOCUMENT_ROOT'] . "/theprojectxxx/models/comments/comme
 
 	
         <div class = "article">
+           <h1> <strong><?=$article['title']?></strong> </h1>
            <div class = "article-main-image-wrap">
                 <img alt="<?=$article['img_main_alt']?>" src="img/articles/article_image-<?=$article['id']?>.jpg" class = "img-responsive pull-left"> 
            </div>
             <div class = "article-header">
-                <h1> <strong><?=$article['title']?></strong> </h1>
+               
+                <!--
                 <em> <?="date_icon &nbsp;" . $article['date'] . "&nbsp;" . "comment_icon 48" ?></em>
-                <em class = "labels"> label_icon Хохлы, бандеры, либерахи</em>
+                <em class = "labels"> <?php echo $article['tag'] ?></em>
+                -->
             </div>
             
             <div class = "article-content">
@@ -43,7 +49,9 @@ require_once ( $_SERVER['DOCUMENT_ROOT'] . "/theprojectxxx/models/comments/comme
             </div>
             
             <div class = "article-footer">
+               
                 <div class='share-social'>
+                   <!--
                     <div class = "vk-share">
                         <script type="text/javascript" src="https://vk.com/js/api/share.js?95" charset="windows-1251"></script>
                     
@@ -60,6 +68,7 @@ require_once ( $_SERVER['DOCUMENT_ROOT'] . "/theprojectxxx/models/comments/comme
                     <div class = "tw-share">
                         <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
                     </div>
+                -->
                 </div>
                 
 
@@ -68,40 +77,62 @@ require_once ( $_SERVER['DOCUMENT_ROOT'] . "/theprojectxxx/models/comments/comme
         </div>
     </div>
     <div class = "col-md-3 hidden-sm hidden-xs">
+        <h3 class = "last-related-articles text-right">ПОСЛЕДНИЕ СТАТЬИ</h3>
         <div class = "article-related">
-            <h3>news list</h3>
-            <?php foreach ($articles as $a): ?>  
+            <!--<h3>Последние статьи</h3>-->
+            <?php 
+                    
+                for ( ($key = 1 ); ($key <  7 ); $key++ ) { 
+                    $a = $articles[ $key ];
+                    
+                    $article_author = get_author_by_article($a['id']);
+                    $comments_number = get_comments_number($a['id']);
+                    $comments_number_noun = "комментариев";
+                    $article_date = $a['date'];
+                    $article_date_array = explode("-", $article_date);
+                    $article_date_array_numeric = $article_date_array;
+                    switch (substr($comments_number, -1)){
+                        case "1":
+                        $comments_number_noun = "комментарий";
+                        break;
+                        case "2":
+                        $comments_number_noun = "комментария";
+                        break;
+                        case "3":
+                        $comments_number_noun = "комментария";
+                        break; 
+                        case "4":
+                        $comments_number_noun = "комментария";
+                        break;    
+                    }
+                ?>  
             <!-- отдельный блок статьи-->
         
             <div class='article-wrap' >                           
-                <div class='image-wrap'> 
-                    <!--<img alt="#0" src="img/test_image4.jpg"> -->
-                   
-                    <div class='post-author'>
-
-                        <div class='image-thumb'>
-                            <img alt='#0' title='#0' src='img/author_icon.jpg'/>
-                            <cite> 
-                                <a href="#0"><?php echo "Author"; ?></a> 
-                                <span><?php echo "{$a['date']}"; ?> </span>
-                            </cite>  
-                        </div>
-                    </div>
-                    
-                </div>
-                
-                <div class='post-body'>
+                              
+                <div class='post-body post-body-related'>
                     <div class='post-title'>
-                        <h2><a href="index.php?send=article&id=<?=$a['id']?>"> <?php echo $a['title'] ?> </a></h2> <!-- Вывод названия статьи, первые 100 символов по дефолту -->
+                        <h2 class = "post-related-title"><a href="index.php?send=article&id=<?=$a['id']?>"> <?php echo $a['title'] ?> </a></h2> <!-- Вывод названия статьи, первые 100 символов по дефолту -->
+                        <h4>
+                            <img class = "related-author-image" alt='#0' title='#0' src='img/avatars/user-<?php echo $article_author['userID'] ?>.jpg'/>   
+                            <a class = "main-page-author-name" href="http://localhost/theprojectxxx/user-<?php echo $article_author['userID'] ?>.html">
+                                <span class = "related-author-name">
+                                    <?php echo $article_author['PublicUserName'] . ", " ?>
+                                </span>
+                                <span class = "related-author-date">   
+                                    <?php echo $article_date_array_numeric[2] . "." . $article_date_array_numeric[1] . "." . $article_date_array_numeric[0] ?>
+                                </span>
+                            </a>
+                        </h4>
                     </div>
 
                     <div class='post-entry'>
-                     <p> <?php echo articles_intro($a['content']) ?></p> <!-- Вывод текста, первые 100 символов по дефолту -->
+                     <p class = "post-related-content"> <?php echo articles_intro($a['content']) ?></p> <!-- Вывод текста, первые 100 символов по дефолту -->
                     </div>
 
                     <div class='postfooter clearfix'>
                        <i class='fa fa-comment linker'></i>
-                        <a class='linker' href="index.php?send=article&id=<?=$a['id']?>#comments" >48 Комментариев</a>
+                        <a class='linker' href="index.php?send=article&id=<?=$a['id']?>#comments" ><?php echo $comments_number . " " . $comments_number_noun ?></a>
                         <!-- Социалки для превью статьи
                             <div class='socialpost'>
                                <div class='icons clearfix'>
@@ -112,13 +143,13 @@ require_once ( $_SERVER['DOCUMENT_ROOT'] . "/theprojectxxx/models/comments/comme
                                
                             </div>
                         --> 
-                        <a href="index.php?send=article&id=<?=$a['id']?>"><div class='read'>Читать </div></a>
+                        <!--<a href="index.php?send=article&id=<?=$a['id']?>"><div class='read'>Читать </div></a>-->
                     </div>
                 </div>
             </div>   
         
         <!-- отдельный блок статьи-->
-        <?php endforeach ?>
+        <?php } ?>
         </div>
     </div>
 </section>
